734fd38bee06d8c0c6d127df5d20df72d1c5a354
Iryna Oleksyshyn <irynkaoleksyshyn@gmail.com>
2023-04-05 20:56:31 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+    const dataJSON = JSON.stringify(data.seconds);
+    localStorage.setItem('videoplayer-current-time', dataJSON);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+    player.setCurrentTime(savedTime);
+}
