2631905355a1cfbc865fdfad319029ca60d42f0d
Iryna Oleksyshyn <irynkaoleksyshyn@gmail.com>
2023-04-06 15:38:09 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const formData = {};
+const DATA_KEY = 'feedback-form-state';
+    
+populateForm();
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(DATA_KEY);
+    console.log(formData);
+}
+
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(DATA_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+    const savedData = localStorage.getItem(DATA_KEY);
+    const savedDataJSON = JSON.parse(savedData);
+    if (savedData) {
+        form.email.value = savedDataJSON.email;
+        form.message.value = savedDataJSON.message;
+    }
+}
